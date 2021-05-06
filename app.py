02ca8beb7d512f6528e4fabbd2a5182fb7bc4590
tabishqidwai98 <60247314+tabishqidwai98@users.noreply.github.com>
2021-05-06 20:15:39 +0530

    ...

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
 import streamlit as st
 import pandas as pd
 
+from analyze import Analyse
 
+analysis = Analyse()
 
 def viewDataset(path):
-    df = pd.read_csv(path)
-    st.dataframe(df)
+    st.header('Data Used in Project')
+    dataframe = analysis.getDataframe()
 
+    with st.spinner("Loading Data..."):
+        st.dataframe(dataframe)
+
+        st.markdown('---')
+        cols = st.beta_columns(4)
+        cols[0].markdown("### No. of Rows :")
+        cols[1].markdown(f"# {dataframe.shape[0]}")
+        cols[2].markdown("### No. of Columns :")
+        cols[3].markdown(f"# {dataframe.shape[1]}")
+        st.markdown('---')
+
+        st.header('Summary')
+        st.dataframe(dataframe.describe())
+        st.markdown('---')
+
+        types = {'object' : 'Categorical', 'int64': 'Numerical', 'float64': 'Numerical'}
+        types = list(map(lambda t: types[str(t)], dataframe.dtypes))
+        st.header('Dataset Columns')
+        for col, t in zip(dataframe.columns, types):
+            st.markdown(f"### {col}")
+            cols = st.beta_columns(4)
+            cols[0].markdown('#### Unique Values :')
+            cols[1].markdown(f"# {dataframe[col].unique().size}")
+            cols[2].markdown('#### Type :')
+            cols[3].markdown(f"## {t}")
 
 sidebar = st.sidebar
 sidebar.header('Choose Your Option')
@@ -18,12 +45,15 @@ if choice == options[0]:
     st.info('lifeExpectancyAtBirth.csv -> Life expectancy at birth, country wise mentioned in age (years).')
     viewDataset('datasets/lifeExpectancyAtBirth.csv')
 elif choice == options[1]:
-    st.image('images/analysisByCountry.png')
-    st.image('images/analysisByCountry1.png')
+    with st.spinner("Loading Data..."):
+        st.image('images/analysisByCountry.png')
+        st.image('images/analysisByCountry1.png')
 elif choice == options[2]:
-    st.image('images/analysisByGender.png')
-    st.image('images/analysisByGender1.png')
-    st.image('images/pieGender.png')
+    with st.spinner("Loading Data..."):
+        st.image('images/analysisByGender.png')
+        st.image('images/analysisByGender1.png')
+        st.image('images/pieGender.png')
 elif choice == options[3]:
-    st.image('images/analysisByYear.png')
-    st.image('images/analysisByYear1.png')
+    with st.spinner("Loading Data..."):
+        st.image('images/analysisByYear.png')
+        st.image('images/analysisByYear1.png')
